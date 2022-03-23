f74d797557bfe42a6ee264a235b7003e7795b1e8
kuebra <karacankubraa@gmail.com>
2022-03-23 10:09:25 +0100

    Script to print the cost of rollouts

---
+++ demo_robot/evaluate_cost_vars.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import numpy as np
+import sys
+import os
+
+from TaskPickTool import *
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2: 
+        task_object = TaskPickTool()
+        directory = sys.argv[1]
+        print(len([i for i, j, k in os.walk(directory)])-1)
+        for l in range(len([i for i, j, k in os.walk(directory)])-1):
+            file = directory + 'rollout0' + str(l+1).zfill(2) + '/cost_vars.txt'
+            cost_vars = np.loadtxt(file)
+            samples = np.zeros((len(cost_vars)))
+            costs = task_object.evaluateRollout(cost_vars, samples)
+            print("Costs Rollout " + str(l+1) + ": " + str(costs))
+    else:
+        print("Usage: evaluate_cost_vars.py + rollout_directory")
+
